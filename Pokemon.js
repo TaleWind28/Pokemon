9053d65da616d4bf98d817504c9d627cdc2af58f
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2022-11-11 21:45:19 +0100

    Create Pokemon.js

---
+++ Pokemon.js
@@ -0,0 +1,33 @@
+const tipi = require('./Tipi')
+
+//interfaccia che crea l'oggetto Pokemon
+class Pokemon{
+    constructor(nome,tipo1,tipo2 = tipo1){
+        this.nome = nome;
+        this.typing = {primo:tipo1.nome,secondo:tipo2.nome};
+    }
+}
+
+class Dex {
+    constructor(){
+        this.dex = {normale:[],lotta:[],elettro:[],folletto:[],buio:[],spettro:[],psico:[],veleno:[],terra:[],ghiaccio:[],fuoco:[],erba:[],acqua:[],roccia:[],drago:[],volante:[],coleottero:[],acciaio:[]}
+    }
+
+    add(pokemon){
+        this.dex[pokemon.typing.primo].push(pokemon);
+        if (pokemon.typing.primo != pokemon.typing.secondo){
+            this.dex[pokemon.typing.secondo].push(pokemon);
+        }
+    }
+}
+pikachu = new Pokemon("Pikachu",tipi.elettro);
+jigglypuff = new Pokemon("Jigglypuff",tipi.normale,tipi.folletto);
+//il dex viene ordinato in base ai tipi pokemon per facilitare l'aggiunta al team;
+var d= new Dex();
+d.add(pikachu);
+d.add(jigglypuff);
+//d.add(donphan);
+//d.add()
+console.log(d);
+exports.dex = d;
+//exports.pokemon = Pokemon;
